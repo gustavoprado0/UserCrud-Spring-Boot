e7f5f4ce2e82b5dc5d61e4d9414c997444c9d625
gustavo.prado <gustavopsz2002@gmail.com>
2026-05-14 16:08:58 -0300

    Add password encryption with BCrypt and add table password for database

--- usercrud/src/main/java/com/gustavo/usercrud/controller/UserController.java
+++ usercrud/src/main/java/com/gustavo/usercrud/controller/UserController.java
@@ -3,7 +3,8 @@ package com.gustavo.usercrud.controller;
 import com.gustavo.usercrud.model.User;
 import com.gustavo.usercrud.repository.UserRepository;
 import org.springframework.web.bind.annotation.*;
-
+import jakarta.validation.Valid;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import java.util.List;
 
 @RestController
@@ -11,13 +12,16 @@ import java.util.List;
 public class UserController {
 
     private final UserRepository repository;
+    private final BCryptPasswordEncoder encoder;
 
-    public UserController(UserRepository repository) {
+    public UserController(UserRepository repository, BCryptPasswordEncoder encoder) {
         this.repository = repository;
+        this.encoder = encoder;
     }
 
     @PostMapping
     public User create(@RequestBody User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
         return repository.save(user);
     }
 
--- usercrud/src/main/java/com/gustavo/usercrud/model/User.java
+++ usercrud/src/main/java/com/gustavo/usercrud/model/User.java
@@ -1,6 +1,7 @@
 package com.gustavo.usercrud.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "users")
@@ -8,15 +9,18 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
+    private Long id;
     private String name;
     private String email;
+    @NotBlank(message = "Password obrigatório")
+    private String password;
     private int age;
 
     public User() {}
 
-    public User(String name, String email, int age) {
+    public User(String name, String email, String password, int age) {
+        this.password = password;
         this.name = name;
         this.email = email;
         this.age = age;
@@ -49,5 +53,17 @@ public class User {
     public void setAge(int age) {
         this.age = age;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
 
 }
